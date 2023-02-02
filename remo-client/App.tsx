230dd21893d3c98731e38bdd5996fa4be86115fb
leoRysing <leshaigor@gmail.com>
2023-02-02 17:29:28 -0500

    updating the alg
    doing backtracking / useStates additions

--- remo-client/App.tsx
+++ remo-client/App.tsx
@@ -2,11 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
+import { AbsoluteCenter } from "@chakra-ui/react";
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  // useState -> barcode
+  const [barcode, setBarcode] = useState<string | null>(null);
 
   useEffect(() => {
     const getBarCodeScannerPermissions = async () => {
@@ -17,41 +18,57 @@ export default function App() {
     getBarCodeScannerPermissions();
   }, []);
 
+  useEffect(() => {
+    console.log(barcode);
+  }, [barcode]);
+
+  // TODO; figure out how to type <type> and <data>
   const handleBarCodeScanned = ({ type, data }) => {
-    // TODO; make a types.d file -> define the value for the type for a barcode
-    // gs1.ean-13
     setScanned(true);
-    // computation
-    let strData = data.toString();
-    if (strData.length == 10) {
-      strData = handleBarcodeConversion(strData);
-    } else {
-      // don't do anything for now
+    // check that this is indeed an isbn-13 or isbn-10 barcode
+    // setBarcode(data);
+    if (type === "org.gs1.EAN-13" && data.substring(0, 3) === "978") {
+      // first, try retrieving the barcode from the database data -> retrieve
+      // we want to error check this later
+      setBarcode(data);
+
+      if (!data) {
+        const isbn10 = handleBarcodeConversion(data);
+        setBarcode(isbn10);
+      }
+
+      // if it still doesn't exist, check the google api
+      if (!data) {
+        // google books implementation
+      }
+    } else if (type === "org.gs1.EAN-13" && data.substring(0, 3) === "979") {
+      setBarcode(data);
+      // if it still doesn't exist, check the google api
+      if (!data) {
+        // google books implementation
+      }
     }
-    // we want to error check this later
-    alert(`Bar code with type ${type} and data ${strData} has been scanned!`);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
   const handleBarcodeConversion = (barcode: string) => {
     // hardcoded length values because this will only be called in instances
-    // of conversion of isbn-10 to isbn-13
-    let res: string = "978" + barcode.substring(0, 9);
+    // of conversion of isbn-13 to isbn-10
+    let res: string = barcode.substring(3, 12);
 
     let sum: number = 0;
+    let multiplier: number = 10;
     // step 3 of the process
     for (var i = 0; i < 12; i++) {
       let tempNum: number;
-      if (i % 2 == 0) {
-        tempNum = +res.substring(i, i + 1);
-      } else {
-        tempNum = +res.substring(i, i + 1) * 3;
-      }
+      tempNum = +res.substring(i, i + 1) * multiplier;
+      multiplier -= 1;
       sum += tempNum;
     }
 
     // step 4
-    const remaind = sum % 10;
-    var lastDig = remaind == 0 ? "0" : (10 - remaind).toString();
+    const remaind = sum % 11;
+    var lastDig = remaind == 0 ? "0" : (11 - remaind).toString();
     res += lastDig;
     return res;
   };
@@ -65,7 +82,6 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={styles.barcodeScanner}
@@ -73,6 +89,7 @@ export default function App() {
       {scanned && (
         <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
       )}
+      <Text style={styles.display}>Barcode: {barcode}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -88,7 +105,12 @@ const styles = StyleSheet.create({
   barcodeScanner: {
     height: "60%",
     width: "100%",
-    position: "absolute",
+    position: "relative",
+    top: -90,
+    left: 0,
+  },
+  display: {
+    position: "relative",
     top: 0,
     left: 0,
   },
