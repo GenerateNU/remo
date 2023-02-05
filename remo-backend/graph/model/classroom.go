476158c518a7e47d65a70ea00f7350c1f37c847f
narayansharma-21 <97.sharman@gmail.com>
2023-02-05 11:02:57 -0500

    Schema Changes
    Schema for all tables should be fully implemented (besides classroom.go). Added all necessary types/inputs/mutations in the schema.graphqls

--- remo-backend/graph/model/classroom.go
+++ remo-backend/graph/model/classroom.go
@@ -1,25 +1,30 @@
 package model
 
+import (
+	"time"
+)
+
 type Classroom struct {
-	classroomId         int `json:"classroomId"`
-	classroom_school_id int `json:"classroom_school_id"`
+	ClassroomId         int `json:"classroomId"`
+	Classroom_school_id int `json:"classroom_school_id"`
 	// DOUBLE-CHECK THE TYPE ON THIS
-	classroom_school_year  string `json:"classroom_school_year"`
-	classroom_start_date   string `json:"classroom_start_date"`
-	classroom_end_date     string `json:"classroom_end_date"`
-	classroom_name         string `json:"classroom_name"`
-	classroom_subject      string `json:"classroom_subject"`
-	classroom_display_name string `json:"classroom_display_name"`
-	classroom_avg_length   string `json:"classroom_avg_length"`
+	Classroom_school_year  time.Time `json:"classroom_school_year"`
+	Classroom_start_date   time.Time `json:"classroom_start_date"`
+	Classroom_end_date     time.Time `json:"classroom_end_date"`
+	Classroom_name         time.Time `json:"classroom_name"`
+	Classroom_subject      time.Time `json:"classroom_subject"`
+	Classroom_display_name time.Time `json:"classroom_display_name"`
+	Classroom_avg_length   time.Time `json:"classroom_avg_length"`
 	// HOW TO DEAL WITH THIS? WHICH TYPES CAN BE ENCODED INTO A JSON?
+	// Might be a way apparently where it just knows which numbers correspond to which values
 	//classroom_avg_frequency int DEFAULT NULL COMMENT '1 = weekly 2 = bi-weekly 3 = monthly 4 = yearly',
-	classroom_avg_days         int    `json:"classroom_avg_days"`
-	classroom_grade_level_type int    `json:"classroom_grade_level_type"`
-	classroom_grade_level      string `json:"classroom_grade_level"`
-	classroom_co_teacher_id    int    `json:"classroom_co_teacher_id"`
-	classroom_teacher_idV1     int    `json:"classroom_teacher_idV1"`
-	classroom_num_students     int    `json:"classroom_num_students"`
-	classroom_num_seats        int    `json:"classroom_num_seats"`
+	Classroom_avg_days         int    `json:"classroom_avg_days"`
+	Classroom_grade_level_type int    `json:"classroom_grade_level_type"`
+	Classroom_grade_level      string `json:"classroom_grade_level"`
+	Classroom_co_teacher_id    int    `json:"classroom_co_teacher_id"`
+	Classroom_teacher_idV1     int    `json:"classroom_teacher_idV1"`
+	Classroom_num_students     int    `json:"classroom_num_students"`
+	Classroom_num_seats        int    `json:"classroom_num_seats"`
 	// HOW TO DEAL WITH THIS? WHICH TYPES CAN BE ENCODED INTO A JSON?
 	//classroom_student_sign_in_mode int DEFAULT NULL COMMENT '1 - Shared (QR or Google code), 2 - Single-user (email/pass).',
 	//classroom_can_share_books int DEFAULT NULL COMMENT '1 = yes, anything else is no.',
@@ -28,8 +33,8 @@ type Classroom struct {
 	//classroom_can_view_teacher_bookshelves int DEFAULT NULL COMMENT '1 = yes, anything else is no.',
 	//classroom_enable_family_access int DEFAULT NULL COMMENT '1 = yes, anything else is no.',
 	//classroom_status_id int NOT NULL DEFAULT '0' COMMENT '0 = inactive, 1= active, 2 =archived',
-	classroom_conf_frequency_above     int `json:"classroom_conf_frequency_above"`
-	classroom_conf_frequency_on        int `json:"classroom_conf_frequency_on"`
-	classroom_conf_frequency_below     int `json:"classroom_conf_frequency_below"`
-	classroom_conf_frequency_far_below int `json:"classroom_conf_frequency_far_below"`
+	Classroom_conf_frequency_above     int `json:"classroom_conf_frequency_above"`
+	Classroom_conf_frequency_on        int `json:"classroom_conf_frequency_on"`
+	Classroom_conf_frequency_below     int `json:"classroom_conf_frequency_below"`
+	Classroom_conf_frequency_far_below int `json:"classroom_conf_frequency_far_below"`
 }
--- remo-backend/graph/model/reading_rate_result.go
+++ remo-backend/graph/model/reading_rate_result.go
@@ -1,36 +1,47 @@
 package model
 
+import (
+	"time"
+)
+
 type ReadingRateResult struct {
-	reading_rate_results_id int `json:"reading_rate_results_id"`
-	user_id                 int `json:"reading_rate_results_id"`
-	book_id                 int `json:"reading_rate_results_id"`
-	user_book_id            int `json:"reading_rate_results_id"`
+	Reading_rate_results_id int `json:"reading_rate_results_id"`
+	User_id                 int `json:"user_id"`
+	Book_id                 int `json:"nook_id"`
+	User_book_id            int `json:"user_book_id"`
 	//`date` datetime DEFAULT NULL,
+	Date time.Time `json:"date"`
 	//`reading_location` int DEFAULT NULL COMMENT '1 = school, 2 = home',
+	Reading_location int `json:"reading_location"`
 	//`type_of_reading` int DEFAULT NULL COMMENT '1=myself,2=someone else, 3=to me, 4= share read',
-	reading_setting int `json:"reading_rate_results_id"`
+	Type_of_reading int `json:"type_of_reading"`
+	Reading_setting int `json:"reading_setting"`
 	//`reading_type_details` int DEFAULT NULL COMMENT '1=Silent, 2=Aloud,3=Adult,4=teen,5=child,6=young child',
+	Reading_type_details int `json:"reading_type_details"`
 	//`start_time` time DEFAULT NULL,
+	Start_time time.Time `json:"start_time"`
 	//`end_time` time DEFAULT NULL,
-	total_time                     int    `json:"reading_rate_results_id"`
-	start_page                     int    `json:"start_page"`
-	end_page                       int    `json:"end_page"`
-	total_pages                    int    `json:"total_pages"`
-	word_per_page                  int    `json:"word_per_page"`
-	types_of_reading               string `json:"types_of_reading"`
-	section_rating                 int    `json:"section_rating"`
-	check_in                       int    `json:"check_in"`
-	reader_request                 int    `json:"reader_request"`
-	reader_request_almost_finished int    `json:"reader_request_almost_finished"`
-	reader_continuum               int    `json:"reader_continuum"`
-	words_per_page                 int    `json:"words_per_page"`
-	observational_notes            string `json:"observational_notes"`
+	End_time                       time.Time `json:"end_time"`
+	Total_time                     int       `json:"Total_time"`
+	Start_page                     int       `json:"start_page"`
+	End_page                       int       `json:"end_page"`
+	Total_pages                    int       `json:"total_pages"`
+	Word_per_page                  int       `json:"word_per_page"`
+	Types_of_reading               string    `json:"types_of_reading"`
+	Section_rating                 int       `json:"section_rating"`
+	Check_in                       int       `json:"check_in"`
+	Reader_request                 int       `json:"reader_request"`
+	Reader_request_almost_finished int       `json:"reader_request_almost_finished"`
+	Reader_continuum               int       `json:"reader_continuum"`
+	Words_per_page                 int       `json:"words_per_page"`
+	Observational_notes            string    `json:"observational_notes"`
 	//`share_feedback` int DEFAULT NULL,
-	reader_response          string `json:"reader_response"`
-	reading_response_pages   string `json:"reading_response_pages"`
-	reading_response_type    int    `json:"reading_response_type"`
-	reading_response_subtype int    `json:"reading_response_subtype"`
-	grade_level              int    `json:"grade_level"`
-	grade_level_status       int    `json:"grade_level_status"`
-	teacher_id               int    `json:"teacher_id"`
+	Share_feedback           int    `json:"share_feedback"`
+	Reader_response          string `json:"reader_response"`
+	Reading_response_pages   string `json:"reading_response_pages"`
+	Reading_response_type    int    `json:"reading_response_type"`
+	Reading_response_subtype int    `json:"reading_response_subtype"`
+	Grade_level              int    `json:"grade_level"`
+	Grade_level_status       int    `json:"grade_level_status"`
+	Teacher_id               int    `json:"teacher_id"`
 }
