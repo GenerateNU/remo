68a98a7213c67084211fb6c3f627896d412c3174
leoRysing <leshaigor@gmail.com>
2023-02-12 15:10:18 -0500

    changes for frontend

--- remo-client/screens/barcodeScanner.tsx
+++ remo-client/screens/barcodeScanner.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
 import { BarcodeResponse } from "../types/index";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import { AbsoluteCenter } from "@chakra-ui/react";
 
 export default function BarcodeScanner() {
@@ -12,6 +12,8 @@ export default function BarcodeScanner() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [barcode, setBarcode] = useState<string | null>(null);
+  const [book, setBook] = useState<JSON | null>();
+  const barcodeEndpoint = "https://localhost:8080/v1/books/:";
 
   const pressHandler = () => {
     navigation.goBack();
@@ -30,6 +32,16 @@ export default function BarcodeScanner() {
     console.log(barcode);
   }, [barcode]);
 
+  const fetchData = async () => {
+    const bookEndpoint = barcodeEndpoint + barcode;
+    console.log(barcode, " -> fetched barcode");
+    const resp = await fetch(bookEndpoint);
+    const data = await resp.json();
+    setBook(data);
+    console.log(data);
+    //setLoading(false);
+  };
+
   // TODO; figure out how to type <type> and <data>
   const handleBarCodeScanned = ({ type, data }: BarcodeResponse) => {
     setScanned(true);
@@ -39,14 +51,18 @@ export default function BarcodeScanner() {
       // first, try retrieving the barcode from the database data -> retrieve
       // we want to error check this later
       setBarcode(data);
+      const bookEndpoint = barcodeEndpoint + barcode;
 
-      if (!data) {
-        const isbn10 = handleBarcodeConversion(data);
-        setBarcode(isbn10);
-      }
+      fetchData();
+
+      // let book = ...;
+      // if (!book) {
+      //   const isbn10 = handleBarcodeConversion(data);
+      //   setBarcode(isbn10);
+      // }
 
       // if it still doesn't exist, check the google api
-      if (!data) {
+      if (!book) {
         // google books implementation
       }
     } else if (type === "org.gs1.EAN-13" && data.substring(0, 3) === "979") {
--- remo-client/screens/home.tsx
+++ remo-client/screens/home.tsx
@@ -1,29 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import BarcodeScanner from './barcodeScanner';
-import { NavigationContainer, useNavigation, Stack } from '@react-navigation/native';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import BarcodeScanner from "./barcodeScanner";
+import {
+  NavigationContainer,
+  useNavigation,
+  Stack,
+} from "@react-navigation/native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function Home({ navigation }) {
-   
+  const pressHandler = () => {
+    navigation.navigate("BarcodeScanner");
+  };
 
-    const pressHandler = () => {
-        navigation.navigate("BarcodeScanner")
-    }
-
-    return (
-        <View style={styles.container}>
-            <Text >Home Screen</Text>
-            <Button title="Scan a Book" onPress={pressHandler}></Button>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button title="Scan a Book" onPress={pressHandler}></Button>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
