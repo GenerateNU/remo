0af67bf8e7ec1a48d9022639295a4c6ce4c2663d
Bedrockdude10 <dannyrollo4@gmail.com>
2023-02-20 19:02:15 -0500

    Able to establish connection to database, need to implement resolvers

--- remo-backend/graph/db_connection_test.go
+++ remo-backend/graph/db_connection_test.go
@@ -12,10 +12,12 @@ func Init() (*sql.DB, error) {
 	USERNAME := os.Getenv("remo")
 	PASSWORD := os.Getenv("pwd")
 	HOST := os.Getenv("127.0.0.1")
-	PORT := os.Getenv("3213")
+	PORT := os.Getenv("3333")
 	DATABASE := os.Getenv("remodb")
 	dbconf := USERNAME + ":" + PASSWORD + "@tcp(" + HOST + ":" + PORT + ")/" + DATABASE + "?charset=utf8mb4" + "&parseTime=True"
-	db, err := sql.Open(os.Getenv("DRIVER"), dbconf)
+	//"mysql"
+	//os.Getenv("DRIVER")
+	db, err := sql.Open("mysql", dbconf)
 	if err != nil {
 		fmt.Printf("Error connecting to database : error= %q\n", err)
 		return nil, err
@@ -23,6 +25,11 @@ func Init() (*sql.DB, error) {
 	return db, err
 }
 
-func TestInitConnection(t *testing.T) {
-	print(Init())
+// Test that we are able to successfully connected to database
+func TestInit(t *testing.T) {
+	var db, err = Init()
+	if err != nil {
+		t.Errorf("Connection was not successfully established to Remo db.")
+	}
+	db.Ping()
 }
