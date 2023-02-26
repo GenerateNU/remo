b07e9e204c5234a2ee6a00e930d423fdc5f3d9ee
tsathyamurthi <tejas.sathyamurthi@gmail.com>
2023-02-26 15:08:20 -0500

    Editing schema resolvers

--- remo-backend/graph/schema.resolvers.go
+++ remo-backend/graph/schema.resolvers.go
@@ -19,16 +19,16 @@ import (
    )
 
 
-func init() (*sql.DB, error) {
+func Init() (*sql.DB, error) {
 		USERNAME := os.Getenv("remo")
 		PASSWORD := os.Getenv("pwd")
 		HOST := os.Getenv("127.0.0.1")
 		PORT := os.Getenv("3213")
 		DATABASE := os.Getenv("remodb")
 	   dbconf := USERNAME + ":" + PASSWORD + "@tcp(" + HOST + ":" + PORT + ")/" + DATABASE + "?charset=utf8mb4" + "&parseTime=True"
-	   db, err := sql.Open(os.Getenv("DRIVER"), dbconf)
+	   db, err := sql.Open(os.Getenv("mysql"), dbconf)
 		if err != nil {
-		 fmt.Println("Error connecting to database : error=%v", err)
+		 fmt.Printf("Error connecting to database : error=%q\n", err)
 		 return nil, err
 		}
 	   return db, err
