674678119caecdeb370c98cbcbff224e13aea75f
brianreicher <reicher.b@northeastern.edu>
2023-02-26 14:39:38 -0500

    Removing bad backend

--- remo-backend/graph/model/book.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Book struct {
-	ID     string `json:"id"`
-	Text   string `json:"text"`
-	Done   bool   `json:"done"`
-	UserID string `json:"userId"`
-	User   *User  `json:"user"`
-}
