ae938f8aee99cc9ea8f4465cd6926cd1cd8a8e45
brianreicher <reicher.b@northeastern.edu>
2023-02-27 12:54:00 -0500

    Cleanup time pt.2

--- remo-backend/src/model/transactions.go
+++ remo-backend/src/model/transactions.go
@@ -53,7 +53,7 @@ func GetUserByEmail(pool *sql.DB, user_email string) (User, error) {
 	err := pool.QueryRow(fmt.Sprintf("SELECT id, first, last FROM logins where email = '%s';", user_email)).Scan(&user.ID, &user.FirstName, &user.LastName)
 
 	if err != nil {
-		return User{FirstName = "Invalid", LastName="User"}, nil
+		return User{}, nil
 	}
 
 	return user, nil
