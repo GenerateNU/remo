0b6b465ae67a931c3387ed842ef338003ef7877f
andrewcaplan1 <andrewcaplan2020@gmail.com>
2023-04-02 16:12:09 -0400

    added search bar and debugged

--- remo-client/screens/bookshelf.tsx
+++ remo-client/screens/bookshelf.tsx
@@ -1,4 +1,4 @@
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React, { useState, useEffect } from "react";
 import {
   View,
@@ -8,30 +8,53 @@ import {
   TouchableHighlight,
   Alert,
 } from "react-native";
+import SearchBar from "../components/searchBar";
+import Data from "../components/mock-data.json";
+
 
 export default function Bookshelf() {
   const [books, setBooks] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
   const [showPopup, setShowPopup] = useState(false);
+  const navigation = useNavigation();
   const route = useRoute();
-  const data = route.params?.data;
+  const paramData = route.params?.data;
+  const [searchText, setSearchText] = useState("");
+  // const [checkedOutBooks, setCheckedOutBooks] = useState({})
+  // const [recentlyReadBooks, setRecentlyReadBooks] = useState({})
 
   useEffect(() => {
-    console.log(data);
+    console.log(paramData);
   }, []);
 
   useEffect(() => {
     // var fetch_string = 'https://e8ac-2601-197-701-1030-e8be-e202-d14c-b4c4.ngrok.io/v1/user_books/' + String({data.id});
-    fetch(`https://bbf3-155-33-132-9.ngrok.io/v1/user_books/${data.id}`).then((response) => response.json())
-      .then((data) => setBooks(data));
+    fetch(`https://19ce-155-33-133-32.ngrok.io/v1/user_books/${paramData.id}`).then((response) => response.json())
+      .then((data) => {setBooks(data)});
   }, []);
-  console.log(data.id);
+  console.log(paramData.id);
   console.log(books);
 
+
+  function filter() {
+    const filteredItems = books.filter((item) =>
+      item.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+    if (filteredItems.length === 0) {
+      return [];
+    } else {
+      return filteredItems;
+    }
+  }
+  
   // TO DO: get proper data representation for 
   // checked out books and recently read books 
-  const checkedOutBooks; 
-  const recentlyReadBooks; 
+  // v1/checkout_book/:bookId/:userId
+  // fetch()
+
+
+  // const checkedOutBooks; 
+  // const recentlyReadBooks; 
 
   return (
     <ScrollView>
@@ -39,14 +62,15 @@ export default function Bookshelf() {
         <Text style={styles.header_title}>Bookshelf</Text>
         <Text style={styles.count}>{books.length} Books</Text>
       </View>
-
+      
+      <SearchBar searchText={searchText} setSearchText={setSearchText} navigation={navigation} data={paramData} />
       {/* Representing the block to handle checkedOutBooks */}
       {//checkedOutBooks.length 
       (
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Checked Out Books</Text>
           <View style={styles.container}>
-            {checkedOutBooks.map((book) => (
+            {filter().map((book) => (
               <TouchableHighlight
                 key={book.id}
                 style={[
@@ -76,7 +100,7 @@ export default function Bookshelf() {
         <View style={styles.section}>
           <Text style={styles.sectionTitle}>Recently Read Books</Text>
           <View style={styles.container}>
-            {recentlyReadBooks.map((book) => (
+            {books.map((book) => (
               <TouchableHighlight
                 key={book.id}
                 style={[
@@ -99,7 +123,7 @@ export default function Bookshelf() {
         </View>
       )}
       
-      <View style={styles.container}>
+      {/* <View style={styles.container}>
         {books.map((book) => (
           <TouchableHighlight
             key={book.id}
@@ -120,7 +144,7 @@ export default function Bookshelf() {
             </>
           </TouchableHighlight>
         ))}
-      </View>
+      </View> */}
       {selectedBook &&
         Alert.alert(
           selectedBook.title,
