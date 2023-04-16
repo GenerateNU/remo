b1bba8dc96a2298f4dafe22b6a54c451f25ff9f0
brianreicher <reicher.b@northeastern.edu>
2023-04-16 18:48:14 -0400

    Updating all books call

--- remo-client/services/book-services.tsx
+++ remo-client/services/book-services.tsx
@@ -28,6 +28,7 @@ export const returnBook = async ({ barcode, user }: BookUser) => {
   console.log(response);
   return response.data;
 };
+
 export const findUserBooks = async (id: string) => {
   const response = await axios.get(`${API_URL}/v1/user_books/${id}`);
 
@@ -46,6 +47,6 @@ export const findUserBooks = async (id: string) => {
 
 export const findAllBooks = async () => {
   const response = await axios.get(`${API_URL}/v1/all_books`);
-  // const response = await axios.get(`localhost:8080/v1/all_books`);
+
   return response.data;
 };
