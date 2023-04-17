e3ea04941064e8a5f6c4274e4e8a463ff947ca9a
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-04-17 16:09:49 -0400

    pause button and hifis

--- remo-client/App.tsx
+++ remo-client/App.tsx
@@ -27,6 +27,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="GoogleSSO" component={GoogleSSO} />
         <Stack.Screen name="Register" component={Register} />
@@ -38,7 +39,14 @@ export default function App() {
         <Stack.Screen name="ReadingLogFlow" component={ReadingLogFlow} />
         <Stack.Screen name="ReadingLog" component={ReadingLog} />
         <Stack.Screen name="BookInfo" component={BookInfo} />
-        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Search" component={SearchScreen}
+        options={{
+          title: 'Find a Book',
+          headerStyle: {
+            backgroundColor: '#954A98',
+          },
+          headerTintColor: '#fff',
+        }}/>
         <Stack.Screen name="AddReadingLog" component={AddReadingLog} />
         <Stack.Screen name="PostReadingLog" component={PostReadingLog} />
         <Stack.Screen name="ReadingLogDisplay" component={ReadingLogDisplay} />
