6dffbd01ebbbdf8f049b2a287feba730a6417b85
leoRysing <leshaigor@gmail.com>
2023-04-17 20:32:39 -0400

    pictures

--- remo-client/screens/readinglog.tsx
+++ remo-client/screens/readinglog.tsx
@@ -6,6 +6,7 @@ import {
   ScrollView,
   TouchableOpacity,
   Alert,
+  Image,
 } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { findUserBooks } from "../services/book-services";
@@ -26,6 +27,7 @@ export default function ReadingLog({ navigation }) {
     let readingLogBooks = await findUserBooks(data.id);
     readingLogBooks = readingLogBooks.slice(0, 4);
     setBooks(readingLogBooks);
+    console.log(readingLogBooks);
   };
 
   const onBookPress = () => {
@@ -36,7 +38,7 @@ export default function ReadingLog({ navigation }) {
       <View style={styles.header}>
         <Text style={styles.header_title}>Currently Reading</Text>
       </View>
-      <ScrollView horizontal={true}>
+      <ScrollView style={{ width: "100%" }}>
         <View style={styles.container}>
           {books.map((book) => (
             <TouchableOpacity
@@ -51,9 +53,12 @@ export default function ReadingLog({ navigation }) {
               }}
             >
               <>
-                <Text style={styles.title}>{book.title}</Text>
-                <Text style={styles.author}>{book.author}</Text>
-                <Text style={styles.isbn}>ISBN-13: {book.isbn_13}</Text>
+                <View style={styles.bookContainer}>
+                  <Image
+                    source={{ uri: book.coverImage }}
+                    style={styles.bookPicture}
+                  />
+                </View>
               </>
             </TouchableOpacity>
           ))}
@@ -67,7 +72,7 @@ export default function ReadingLog({ navigation }) {
       <View style={styles.header}>
         <Text style={styles.header_title}>My Reading Log Reports</Text>
       </View>
-      <ScrollView horizontal={true}>
+      <ScrollView style={{ width: "100%" }}>
         <View style={styles.container}>
           {books.map((book) => (
             <TouchableOpacity
@@ -94,9 +99,14 @@ export default function ReadingLog({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  bookContainer: {
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
     flexDirection: "row",
+    width: "100%",
     flexWrap: "wrap",
     justifyContent: "space-between",
     padding: 20,
@@ -110,6 +120,11 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
   },
+  bookPicture: {
+    width: "100%",
+    height: 180,
+    resizeMode: "contain",
+  },
   header_title: {
     fontSize: 24,
     fontWeight: "bold",
@@ -118,7 +133,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   book: {
-    width: "22%",
+    width: "45%",
     marginBottom: 5,
     borderWidth: 1,
     borderColor: "#ccc",
